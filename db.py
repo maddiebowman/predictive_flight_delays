eb9545eaa7be44b2919afd81fcd319a423219e5a
Thomas Ngo <thomasjngo@live.com>
2024-07-16 10:45:07 -0700

    flask skeleton

---
+++ db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+engine=create_engine('postgresql://postgres:postgres@localhost:5432/flightpredict')
+conn=engine.connect()
+
+# Reading first csv file for drivers
+flight_df=pd.read_csv('data/full_data_flightdelay.csv')
+# creates schema and inserts records
+flight_df.to_sql('flight', if_exists='replace', con=conn)
+
+conn.close()
