f278f5003512b883f577d34a3cf6c3ea4e9c4da2
belindaho2828 <belinda.ho2828@gmail.com>
2024-07-19 15:13:56 -0700

    updated for detailed forecast in weather api

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine, text, inspect
-from weather_function import origin_fcstfn #importing function from separate file
+from weather_function import origin_fcstfn, destination_fcstfn #importing function from separate file
 from plane_function import aircraft_age
 from flask_cors import CORS
+from datetime import date, datetime
 
 app = Flask(__name__)
 engine=create_engine('postgresql://postgres:postgres@localhost:5432/flightpredict', echo=True)
 CORS(app)
+
+today = date.today()
 
 # Setting up homepage that displays the API routes
 @app.route("/")
@@ -90,20 +93,30 @@ def geo_data(offset):
 def show_visuals():
     return render_template('visualize.html')
 
-@app.route('/weather/<date>/<origination>/')
-def weather(date, origination):
-
-    #example date 2024-07-17; example origination LAX
-    forecast_data = origin_fcstfn(date, origination)
-
-    return jsonify(forecast_data)
+#weather at origination
+@app.route('/weather/<flight_date>/<origination>/')
+# Returns the current local date
 
 
+def weather(flight_date, origination):
+    
 
-@app.route('/plane/<date>/<flight_num>/')
-def plane(date, flight_num):
+    try:
+            flight_date_obj = datetime.strptime(flight_date, "%Y-%m-%d").date()
+    except ValueError:
+            return 'Invalid date format. Please use YYYY-MM-DD.', 400
+
+    if flight_date_obj < today:
+        return 'Date is in the past. Please enter a date within a 7-day range.', 400
+    else:
+        forecast_data = origin_fcstfn(flight_date, origination)
+        return jsonify(forecast_data)
+
+
+@app.route('/plane/<flight_date>/<flight_num>/')
+def plane(flight_date, flight_num):
     #example date 2024-07-17; example flight_num wn658
-    plane_data = aircraft_age(date, flight_num)
+    plane_data = aircraft_age(flight_date, flight_num)
 
     return jsonify(plane_data)
 
